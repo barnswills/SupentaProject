360674042b3ae015183c91bea994fcfc5723f5ac
Barney Williams <barneywilliams90@hotmail.co.uk>
2015-02-02 22:14:29 +0000

    toasts successfully displaying when an app is either updated, installed or uninstalled

--- app/src/main/java/android/bwapps/com/supentaproject/MainActivity.java
+++ app/src/main/java/android/bwapps/com/supentaproject/MainActivity.java
@@ -1,14 +1,11 @@
 package android.bwapps.com.supentaproject;
 
 import android.app.Activity;
-import android.app.Notification;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
-import android.support.v4.app.NotificationCompat;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
@@ -49,13 +46,30 @@ public class MainActivity extends Activity
         installAmountText.setText(String.valueOf(installAmountInt));
         uninstallAmountText.setText(String.valueOf(uninstallAmountInt));
 
-        BroadcastReceiver receiver = new BroadcastReceiver()
+        final BroadcastReceiver receiver = new BroadcastReceiver()
         {
+            Outcome outcome = new Outcome();
+
             @Override
             public void onReceive(Context context, Intent intent)
             {
+                if(intent.getAction().equals(Intent.ACTION_PACKAGE_ADDED))
+                {
+                    outcome.SetOutcome("installed");
+                }
+
+                if(intent.getAction().equals(Intent.ACTION_PACKAGE_CHANGED))
+                {
+                    outcome.SetOutcome("updated");
+                }
+
+                if(intent.getAction().equals(Intent.ACTION_PACKAGE_REMOVED))
+                {
+                    outcome.SetOutcome("uninstalled");
+                }
+
                 Toast.makeText(getBaseContext(),
-                        "An Application has been installed!",
+                        "An Application has been " + outcome.GetOutcome(),
                         Toast.LENGTH_LONG).show();
             }
         };
---
+++ app/src/main/java/android/bwapps/com/supentaproject/Outcome.java
@@ -0,0 +1,16 @@
+package android.bwapps.com.supentaproject;
+
+public class Outcome
+{
+    private String outcomeString = "";
+
+    public void SetOutcome(String s)
+    {
+        outcomeString = s;
+    }
+
+    public String GetOutcome()
+    {
+        return outcomeString;
+    }
+}
