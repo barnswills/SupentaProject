8ec5f3562be819fb7b0abbf3481fd7b6ef7d734e
Barney Williams <barneywilliams90@hotmail.co.uk>
2015-02-02 17:28:47 +0000

    full gui finished, adding methods and functionality

--- app/src/main/java/android/bwapps/com/supentaproject/MainActivity.java
+++ app/src/main/java/android/bwapps/com/supentaproject/MainActivity.java
@@ -2,38 +2,70 @@ package android.bwapps.com.supentaproject;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity
+{
+    private Button serviceButton;
+    private boolean hasStarted = false;
+
+    private ListView activityList;
+
+    private TextView installAmountText;
+    private TextView uninstallAmountText;
+
+    private int installAmountInt = 0;
+    private int uninstallAmountInt = 0;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        serviceButton = (Button) findViewById(R.id.service_button);
+        serviceButton.setTag(1);
+
+        activityList = (ListView) findViewById(R.id.activity_list);
+
+        installAmountText = (TextView) findViewById(R.id.apps_installed_amount);
+        uninstallAmountText = (TextView) findViewById(R.id.apps_uninstalled_amount);
+
+        installAmountText.setText(String.valueOf(installAmountInt));
+        uninstallAmountText.setText(String.valueOf(uninstallAmountInt));
+
+        monitorApps();
     }
 
+    private void monitorApps()
+    {
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    public void onServiceClick(View view)
+    {
+        final int status = (Integer) view.getTag();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if (status == 1)
+        {
+            // Service will start when button is clicked
+            hasStarted = true;
+            // Buttons text is changed giving the user the option to stop
+            serviceButton.setText(R.string.button_text_stop);
+            view.setTag(0);
+
         }
-
-        return super.onOptionsItemSelected(item);
+        else
+        {
+            // Service will stop when button is clicked
+            hasStarted = false;
+            // Buttons text is changed giving the user the option to start
+            serviceButton.setText(R.string.button_text_start);
+            view.setTag(1);
+        }
     }
 }
